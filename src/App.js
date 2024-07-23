2b2cb37d1a98ef54c8a65922c81116080d53079c
ugbeadie <ugbeadie3@gmail.com>
2024-07-23 18:20:21 +0100

    begin state management

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import React from "react";
-// import "./index.css";
 import Sidebar from "./sidebar/Sidebar";
 import Nav from "./navigation/Nav";
 import Products from "./products/Products";
 import Recommended from "./recommended/Recommended";
+import products from "./db/data";
 
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  // ----------- Input Filter -----------
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const filteredItems = products.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+
+  // ----------- Radio Filtering -----------
+  const handleChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  // ------------ Button Filtering -----------
+  const handleClick = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
   return (
     <>
       <Sidebar />
