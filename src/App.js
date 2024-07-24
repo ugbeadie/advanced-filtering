245c12a9b90ca8db3332de2294d79272bb85710d
ugbeadie <ugbeadie3@gmail.com>
2024-07-24 11:13:58 +0100

    filtering functionality

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Nav from "./navigation/Nav";
 import Products from "./products/Products";
 import Recommended from "./recommended/Recommended";
 import products from "./db/data";
+import Card from "./components/Card";
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [query, setQuery] = useState("");
 
   // ----------- Input Filter -----------
-  const [query, setQuery] = useState("");
 
   const handleInputChange = (e) => {
     setQuery(e.target.value);
@@ -29,6 +30,39 @@ const App = () => {
   const handleClick = (e) => {
     setSelectedCategory(e.target.value);
   };
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
+    // Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    // Applying selected filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
+  }
 
   return (
     <>
